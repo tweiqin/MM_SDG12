879695430050cc020cfd81582b1858e5536aeb55
tweiqin <qinqintay03@gmail.com>
2025-12-23 16:50:04 +0800

    Fix db.php to use environment variables instead of hardcoded creds

--- config/db.php
+++ config/db.php
@@ -1,18 +1,15 @@
 <?php
-// Terraform will output the RDS address, or you can use an environment variable
-$servername = getenv('DB_HOST') ?: "makanmystery-db.your-endpoint.aws.com";
-$username = "admin";
-$password = "030219#Mm";
-$database = "mm_sdg12";
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
+$port = getenv('DB_PORT') ?: 3306;
 
-$conn = new mysqli($servername, $username, $password, $database);
-?>
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-<?php
-$servername = getenv('DB_HOST') ?: "localhost";
-$username = "admin";
-$password = "030219#Mm";
-$dbname = "mm_sdg12";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
